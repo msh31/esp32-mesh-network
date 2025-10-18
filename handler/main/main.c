ceb8dffc5f09d64c5d34862f1a30ff2891428a38
msh31 <180540729+msh31@users.noreply.github.com>
2025-10-18 15:23:57 +0200

    feat: add 2 tasks
    both tasks run on core 0, matter of fact; everything does right now.
    it's an easy adjustment thhough!

--- handler/main/main.c
+++ handler/main/main.c
@@ -2,13 +2,25 @@
 #include "freertos/FreeRTOS.h" // open-source real-0time OS on the ESP32
 #include "freertos/task.h"
 
-void app_main(void) {
-    printf("Handler is starting...\n");
-
-    while(1) {
-        printf("Hello!\n");
-
-        //sleeps for 2 seconds, converts the amount of ms to ticks the ESP32 understands
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
+void test_task(void *pvParameters) {
+    printf("Running on core %d\n", xPortGetCoreID());
+    while (1) {
+        printf("The Test Task executed!\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
+}
+
+void another_task(void *pvParameters) {
+    printf("Running on core %d\n", xPortGetCoreID());
+    while (1) {
+        printf("Another Task has executed!\n");
+        vTaskDelay(1500 / portTICK_PERIOD_MS);
+    }
+}
+
+void app_main(void) {
+    printf("Executing tasks...\n");
+
+    xTaskCreate(test_task, "TestTask", 2048, NULL, 5, NULL);
+    xTaskCreate(another_task, "AnotherTask", 2048, NULL, 5, NULL);
 }
