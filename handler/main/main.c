41661023fcf977b7daeff46f1f51058f3078c280
Marco <180540729+msh31@users.noreply.github.com>
2025-10-20 00:45:11 +0200

    feat: initialize, configure & start Wi-Fi driver
    very cool, it's set to station mode (to connect to another network) and has some basic built-ini error handling now

--- handler/main/main.c
+++ handler/main/main.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
+
+#include "esp_wifi_types_generic.h"
 #include "freertos/FreeRTOS.h" // open-source real-0time OS on the ESP32
 #include "freertos/task.h"
 
-void test_task(void *pvParameters) {
-    printf("Running on core %d\n", xPortGetCoreID());
-    while (1) {
-        printf("The Test Task executed!\n");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-}
-
-void another_task(void *pvParameters) {
-    printf("Running on core %d\n", xPortGetCoreID());
-    while (1) {
-        printf("Another Task has executed!\n");
-        vTaskDelay(1500 / portTICK_PERIOD_MS);
-    }
-}
+#include "nvs_flash.h"
+#include "esp_wifi.h"
+#include "esp_now.h"
+#include "esp_netif.h"
+#include "esp_event.h"
 
 void app_main(void) {
-    printf("Executing tasks...\n");
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    xTaskCreate(test_task, "TestTask", 2048, NULL, 5, NULL);
-    xTaskCreate(another_task, "AnotherTask", 2048, NULL, 5, NULL);
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_wifi_set_mode(WIFI_MODE_STA);
+
+    esp_wifi_start();
+
+    printf("Wifi Initialized!\n");
 }
