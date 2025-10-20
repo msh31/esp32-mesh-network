0df9d786dee19fc5c5d0191583a4e749f84153d5
Marco <180540729+msh31@users.noreply.github.com>
2025-10-20 11:37:53 +0200

    feat: initialize ESP-NOW

--- handler/main/main.c
+++ handler/main/main.c
@@ -14,6 +14,7 @@ void app_main(void) {
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(esp_now_init());
 
     esp_netif_create_default_wifi_sta();
 
