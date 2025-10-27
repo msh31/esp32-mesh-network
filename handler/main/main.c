180bc8c492b076c2f6af0311f7fd5a38270540cd
msh31 <180540729+msh31@users.noreply.github.com>
2025-10-27 11:21:19 +0100

    fix: esp now init order
    esp now after wifi, makes sense. idk why i didnt do it before. i suck at
    programming

--- handler/main/main.c
+++ handler/main/main.c
@@ -14,16 +14,15 @@ void app_main(void) {
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    ESP_ERROR_CHECK(esp_now_init());
 
     esp_netif_create_default_wifi_sta();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
     esp_wifi_set_mode(WIFI_MODE_STA);
-
     esp_wifi_start();
+
+    ESP_ERROR_CHECK(esp_now_init());
 
     printf("Wifi Initialized!\n");
 }
