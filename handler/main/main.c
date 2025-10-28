d8a11ed7576e8f42185b5147bc021a73db6747a8
msh31 <180540729+msh31@users.noreply.github.com>
2025-10-28 13:27:21 +0100

    feat: add on_data_recv callback (esp-now)
    that func gets called when esp-now receives a msg

--- handler/main/main.c
+++ handler/main/main.c
@@ -1,4 +1,6 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "esp_wifi_types_generic.h"
 #include "freertos/FreeRTOS.h" // open-source real-0time OS on the ESP32
@@ -9,6 +11,27 @@
 #include "esp_now.h"
 #include "esp_netif.h"
 #include "esp_event.h"
+
+// 64 bytes, 1 for the type, 3 for the data sent alongside
+// typedef is just an alias to we can use 'Message' instead of 'struct Message' 
+typedef struct {
+    uint8_t type; 
+    uint8_t data[63];
+} Message;
+
+void on_data_recv(const esp_now_recv_info_t *info, const uint8_t *data, int len) {
+    const Message *msg = (const Message *)data;
+
+    if(len != sizeof(Message)) {
+        printf("Received invalid message.");
+        return;
+    }
+
+    printf("Type: %u, first data byte: %u\n", msg->type, msg->data[0]);
+
+    // info->src_addr contains the sender's MAC address
+    // data contains the message bytes
+}
 
 void app_main(void) {
     ESP_ERROR_CHECK(nvs_flash_init());
@@ -23,6 +46,8 @@ void app_main(void) {
     esp_wifi_start();
 
     ESP_ERROR_CHECK(esp_now_init());
+
+    esp_now_register_recv_cb(on_data_recv);
 
     printf("Wifi Initialized!\n");
 }
